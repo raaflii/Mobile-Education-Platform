c50dbf40698743c09d2146acde21c0784e796127
raaflii <rafliwrzky@gmail.com>
2025-06-14 16:46:41 +0700

    add auth repo

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:mobile_education_platform/features/auth/domain/entitites/user.dart';
+import 'package:mobile_education_platform/features/auth/domain/failures/auth_failure.dart';
+
+abstract class AuthRepository {
+  // Authentication methods
+  Future<Either<AuthFailure, User>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+  
+  Future<Either<AuthFailure, User>> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String username,
+    required UserRole role,
+  });
+  
+  Future<Either<AuthFailure, Unit>> sendPasswordResetEmail({
+    required String email,
+  });
+  
+  Future<Either<AuthFailure, Unit>> signOut();
+  
+  // User data methods
+  Future<Either<AuthFailure, User?>> getCurrentUser();
+  Stream<User?> get authStateChanges;
+  
+  // Username validation
+  Future<Either<AuthFailure, bool>> isUsernameAvailable(String username);
+}
